a27e267d9537ef8e0b7153830a03d81522e87863
gokulv01 <85456993+gokulv01@users.noreply.github.com>
2021-06-07 00:45:29 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer(){
+
+var currYear=new Date().getFullYear();
+
+   return(
+         <footer>
+                   <p>Copyright a{currYear}</p> 
+                             </footer>
+                                );
+                                }
+
+
+
+                                 export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note(){
+  return(
+      <div className="note">
+            <h1>Javascript and react.js</h1>
+                  <p>
+                      It was very informative session. Great experience attending this course.
+                            </p>
+                            </div>
+                              );
+                                }
+
+                                export default Note;
